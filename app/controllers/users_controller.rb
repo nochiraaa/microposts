fd32670d9e174469937f28a307015abaed5df430
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-31 08:02:25 +0000

    before followings/follower make

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:edit, :update]  
+  before_action :set_user, only: [:edit, :update, :followings, :followers]  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts
@@ -31,6 +31,14 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def followings
+    render 'followings'
+  end
+  
+  def followers
+    render 'followers'
   end
   
   private 
