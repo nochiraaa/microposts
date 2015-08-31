c7f1b273b71e2b631738cf2badfd01967fa5e3cf
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-31 07:40:14 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,22 @@ class User < ActiveRecord::Base
     
     has_secure_password
     has_many :microposts
+    
+    def follow(other_user)
+        following_relationships.create(followed_id: other_user.id)
+    end
+    
+    def unfollow(other_user)
+        following_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
+    
+    
+    has_many :following_relationships, class_name: "Relationship",
+                                        foreign_key: "follower_id",
+                                        dependent: :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
 end
--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,12 @@ module Microposts
 
     config.generators do |g|
         g.stylesheets false
-	g.javascripts false
-	g.helper false
-	g.test_framework false
+      	g.javascripts false
+      	g.helper false
+      	g.test_framework false
     end
+    
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    
   end
 end
