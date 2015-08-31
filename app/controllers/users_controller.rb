609bc7d570ff619f43a4e4463b69308177255a9b
Yuki Aihara <aiharayuki0702@gmail.com>
2015-08-31 09:45:26 +0000

    implementation followingspage and followerpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :followings, :followers]  
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'users/:id/followings', to:'users#followings', as: 'followings'
+  get 'users/:id/followers', to:'users#followers', as: 'followers'
   
   resources :users
   resources :microposts
   resources :sessions, only: [:new, :create, :destroy]
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy, :followings, :followers]
 end
